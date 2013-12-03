0064e9faed0e5114ec3d621708edbdfb2c7729b8
pkorenev <pasha_kor@mail.ru>
2013-12-03 16:50:01 +0200

    test for page controller

---
+++ test/controllers/page_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PageControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get custom_page" do
+    get :custom_page
+    assert_response :success
+  end
+
+end
