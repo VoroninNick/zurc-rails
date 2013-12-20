ea1283a6b92f4abb74a7dd821dac7358711f9dbc
pasha <p.korenev@voroninstudio.eu>
2013-12-20 20:13:48 +0200

    update

--- db/migrate/20131218214410_init_db.rb
+++ db/migrate/20131218214410_init_db.rb
@@ -5,7 +5,7 @@ class InitDb < ActiveRecord::Migration
     # create user
     query = 'INSERT INTO "users" ("created_at", "email", "encrypted_password", "updated_at") VALUES (\'Wed, 18 Dec 2013 21:31:10 \', \'pasha_kor@mail.ru\', \'$2a$10$BL7RXFrOap75RRat8LRarOCmwHI1tuUNDIazyclhHyEmxpiD/.kji\', \'Wed, 18 Dec 2013 21:31:10 UTC +00:00\' )'
     #ActiveRecord::Base.connection.execute( query )
-    User.create!(created_at: 'Wed, 18 Dec 2013 21:31:10 UTC +00:00', email: 'pasha_kor@mail.ru', password: 'pasha1991', updated_at: 'Wed, 18 Dec 2013 21:31:10 UTC +00:00')
+    #User.create!(created_at: 'Wed, 18 Dec 2013 21:31:10 UTC +00:00', email: 'pasha_kor@mail.ru', password: 'pasha1991', updated_at: 'Wed, 18 Dec 2013 21:31:10 UTC +00:00')
 
 
     # init HomeGalleryImages
