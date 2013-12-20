b572c4270b7efeb71d67f049bc94f8f3ee102f3c
pasha <p.korenev@voroninstudio.eu>
2013-12-20 19:38:28 +0200

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password
+  attr_accessible :email, :password, :created_at, :updated_at
 end
