82a20c171dc273ff663c13423eed1fdcc47d479f
pasha <p.korenev@voroninstudio.eu>
2013-12-26 18:03:01 +0200

    v1

--- db/migrate/20131218214410_init_db.rb
+++ db/migrate/20131218214410_init_db.rb
@@ -15,25 +15,25 @@ class InitDb < ActiveRecord::Migration
 
 
     MainMenuItem.create!( name: 'root_news', title: 'Новини', parent: nil, link: '/news' )
-    MainMenuItem.create!( name: 'root_about', title: 'Про нас', parent: nil, link: nil )
+    MainMenuItem.create!( name: 'root_about', title: 'Про нас', parent: nil, link: '' )
     MainMenuItem.create!( name: 'root_what_we_do', title: 'Що ми робимо', parent: nil, link: '/what-we-do' )
     MainMenuItem.create!( name: 'root_publications', title: 'Публікації', parent: nil, link: '/publications' )
     MainMenuItem.create!( name: 'root_contact', title: 'Контакти', parent: nil, link: '/contact' )
 
-    MainMenuItem.create!( name: 'about_mission', title: 'Місія', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_geography', title: 'Географія', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_structure', title: 'Організаційна структура', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_history', title: 'Історія', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_active_projects', title: 'Діючі проекти', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_complete_projects', title: 'Реалізовані проекти', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_reports', title: 'Річні звіти', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
-    MainMenuItem.create!( name: 'about_team', title: 'Наша команда', parent: MainMenuItem.find_by_name( 'root_about' ), link: nil )
+    MainMenuItem.create!( name: 'about_mission', title: 'Місія', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_geography', title: 'Географія', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_structure', title: 'Організаційна структура', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_history', title: 'Історія', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_active_projects', title: 'Діючі проекти', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_complete_projects', title: 'Реалізовані проекти', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_reports', title: 'Річні звіти', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
+    MainMenuItem.create!( name: 'about_team', title: 'Наша команда', parent: MainMenuItem.find_by_name( 'root_about' ), link: '' )
 
-    MainMenuItem.create!( name: 'what_society_development', title: 'Розвиток громади', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: nil )
-    MainMenuItem.create!( name: 'what_resource_center', title: 'Ресурсний центр для ОСН', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: nil )
-    MainMenuItem.create!( name: 'what_andragogics', title: 'Андрагогіка', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: nil )
-    MainMenuItem.create!( name: 'what_migration', title: 'Міграція', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: nil )
-    MainMenuItem.create!( name: 'what_explore', title: 'Дослідження', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: nil )
+    MainMenuItem.create!( name: 'what_society_development', title: 'Розвиток громади', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: '' )
+    MainMenuItem.create!( name: 'what_resource_center', title: 'Ресурсний центр для ОСН', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: '' )
+    MainMenuItem.create!( name: 'what_andragogics', title: 'Андрагогіка', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: '' )
+    MainMenuItem.create!( name: 'what_migration', title: 'Міграція', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: '' )
+    MainMenuItem.create!( name: 'what_explore', title: 'Дослідження', parent: MainMenuItem.find_by_name( 'root_what_we_do' ), link: '' )
 
 
   end
